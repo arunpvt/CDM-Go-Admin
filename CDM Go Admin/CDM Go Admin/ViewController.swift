7e8f6632d54fd8202f41a74aaf54e4fd5edb3646
c-expansion-mbalaji_turner <madhusudhanarao.balaji@turner.com>
2015-08-28 10:44:24 +0530

    Initial Commit

---
+++ CDM Go Admin/CDM Go Admin/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  CDM Go Admin
+//
+//  Created by Developer on 28/08/15.
+//  Copyright (c) 2015 Developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
